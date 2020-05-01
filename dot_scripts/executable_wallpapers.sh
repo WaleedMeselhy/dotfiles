de3f56541515e1098acb16b7db04d03487fe564e
Waleed Meselhy <waleed.meselhy@nana.sa>
2020-05-01 21:00:06 +0200

    add script to download wallpapers

---
+++ dot_scripts/executable_wallpapers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RESOLUTION="2560x1440"
+URL="https://source.unsplash.com/featured/${RESOLUTION}/?cyberpunk"
+WALLPAPER=~/.images/wallpaper.jpeg
+
+if test -f "$WALLPAPER"; then
+    rm "$WALLPAPER"
+fi
+mkdir -p "~/.images/"
+RES=$(curl -L "$URL" -o "$WALLPAPER")
+if [ -z "$RES" ]; then
+    feh --bg-scale "$WALLPAPER"
+fi
